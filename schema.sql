e0f700dfcae86a97d1482387f2fcd30257587a73
snigdha <snigdha.dagar@gmail.com>
2015-02-24 22:49:15 +0530

    Added a filenames table
    Separated the filenames from the entries table.

--- schema.sql
+++ schema.sql
@@ -1,14 +1,15 @@
 create table if not exists users (
-		userid integer primary key autoincrement,
-		name string not null,
-		username string not null,
-		password string not null,
-		email string not null
-		);
-create table if not exists entries (
-		id integer primary key autoincrement,
-		title string not null,
-		text string not null,
-		username string not null,
-		filename string
-		);
+        userid integer primary key autoincrement,
+        name string not null,
+        username string not null,
+        password string not null,
+        email string not null
+        );
+create table if not exists entries ( id integer primary key autoincrement, title string not null,
+        text string not null,
+        username string not null
+        );
+create table if not exists filenames (
+        id integer primary key,
+        filename string not null
+        );
