139d773cbf6c1678a4722329ab08c010b7cccedc
snigdha <snigdha.dagar@gmail.com>
2015-03-07 22:55:10 +0530

    Updated schema.sql
    Added timestamp field to the entries table.

--- schema.sql
+++ schema.sql
@@ -1,13 +1,16 @@
 create table if not exists users (
         userid integer primary key autoincrement,
         name string not null,
-        username string not null,
+        author string not null,
         password string not null,
         email string not null
         );
-create table if not exists entries ( id integer primary key autoincrement, title string not null,
+create table if not exists entries (
+        id integer primary key autoincrement,
+        title string not null,
         text string not null,
-        username string not null
+        author string not null,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
         );
 create table if not exists filenames (
         id integer primary key,
