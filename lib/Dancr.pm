a064dc382dfb280a11034a7d1a809951e03e0607
snigdha <snigdha.dagar@gmail.com>
2015-03-07 22:54:29 +0530

    Added function to display feeds
    The feed displays the last 5 posts on from the database. Currently, its displaying only the title for each entry, I will get it to display the others in the next commit.

--- lib/Dancr.pm
+++ lib/Dancr.pm
@@ -6,8 +6,11 @@ use Dancer2;
 use DBI;
 use File::Spec;
 use File::Slurp;
+use Template;
 use Dancer2::Plugin::Auth::Tiny;
 use Dancer2::Plugin::Passphrase;
+use Dancer2::Plugin::Feed;
+use POSIX qw/strftime/;
 
 set 'public_dir' => '/home/snigdha/Dancr/public';
 set 'upload_dir' => '/uploadsFolder/';
@@ -55,15 +58,16 @@ hook before_template => sub {
     $tokens->{'login_url'} = uri_for('/login');
     $tokens->{'register_url'} = uri_for('/register');
     $tokens->{'logout_url'} = uri_for('/logout');
+    $tokens->{'feed_url'} = uri_for('/feed');
 };
 
 get '/' => needs login => sub {
     if ( not session('logged_in') ){
-        print "here\n";
+        #needed to maintain session state
     }
 
     my $db = connect_db();
-    my $sql = 'select id, title, text, username from entries order by id desc';
+    my $sql = 'select id, title, text, author from entries order by id desc';
     my $sth = $db->prepare($sql) or die $db->errstr;
     $sth->execute or die $sth->errstr;
     $sql = 'select id,filename from filenames order by id desc';
@@ -87,15 +91,15 @@ post '/add' => needs login => sub {
     my $upload = request->upload('file');
     my $fname;
 
-    my $sql = 'insert into entries (title, text, username) values (?, ?, ?)';
+    my $sql = 'insert into entries (title, text, author, timestamp) values (?, ?, ?, ?)';
     my $sth = $db->prepare($sql) or die $db->errstr;
-    $sth->execute(params->{'title'}, params->{'text'}, session('user')) or die $sth->errstr;
-
+    my $date = strftime('%Y-%m-%d %T',localtime);
+    $sth->execute(params->{'title'}, params->{'text'}, session('user'), $date) or die $sth->errstr;
     if($upload){
         $fname = setting('upload_dir').$upload->filename;
         $upload->copy_to(setting('public_dir').$fname);
 
-        $sql = "select id from entries where username=? and title =? and text=?";
+        $sql = "select id from entries where author =? and title =? and text=?";
         my @row = $db->selectrow_array($sql,undef,session('user'),params->{'title'},params->{'text'});
 
         $sql = 'insert into filenames (id, filename) values (?, ?)';
@@ -109,13 +113,17 @@ post '/add' => needs login => sub {
 any ['get' ,'post'] => '/edit' => needs login =>  sub {
     session('user'); #without this line tiny auth redirects to login page again and again
     my $db  = connect_db();
-    my $sql = "SELECT title, text, username FROM entries WHERE id=?";
-    my @row = $db->selectrow_array($sql,undef,params->{'rowid'});
+    my $sql = "SELECT title, text, author FROM entries WHERE id=?";
+    my $sth = $db->prepare($sql) or die $db->errstr;
+    $sth->execute(params->{'rowid'});
+    my @row = $sth->fetchrow_array();
+    # my @row = $db->selectrow_array($sql,undef,params->{'rowid'});
     
     if ( $row[2] ne session('user')) {
         set_flash('Unauthorized access');
         redirect '/';
     }
+
     template 'edit.tt', {
         'title_value'      => $row[0],
         'text_value'       => $row[1],
@@ -129,13 +137,16 @@ post '/update' => needs login => sub{
     my $sql = 'update entries set title=?, text=? where id=?';
     my $sth = $db->prepare($sql) or die $db->errstr;
     $sth->execute(params->{'title'}, params->{'text'}, params->{'rowid'}) or die $sth->errstr;
+    my @row = $sth->fetchrow_array();
+   
+
     set_flash('Entry updated!');
     redirect '/';
 };
 
 post '/delete' => needs login => sub{
     my $db  = connect_db();
-    my $sql = "SELECT username FROM entries WHERE id=?";
+    my $sql = "SELECT author FROM entries WHERE id=?";
     my @row = $db->selectrow_array($sql,undef,params->{'rowid'});
     
     if ( $row[0] ne session('user')) {
@@ -163,11 +174,11 @@ any ['get', 'post'] => '/login' => sub {
     if ( request->method() eq "POST" ) {
         # process form input
         my $db  = connect_db();
-        my $sql = 'select username,password from users where username=?';
+        my $sql = 'select author,password from users where author=?';
         my @ans = $db->selectrow_array($sql, undef, params->{'username'});
         my ($user,$pwd) = @ans;
         if (params->{'username'} ne $user){
-            $err = 'Invalid username';
+            $err = 'Invalid author';
         }
         elsif(not passphrase(params->{'password'})->matches($pwd)) {
             $err = 'Invalid password';
@@ -176,7 +187,6 @@ any ['get', 'post'] => '/login' => sub {
             session 'user' => params->{'username'};
             session 'logged_in' => true;
             set_flash('You are logged in.');
-            print params->{return_url};
             return redirect params->{return_url} || '/';
             #return redirect '/';
         }
@@ -192,7 +202,7 @@ any ['get', 'post'] => '/register' => sub {
     if ( request->method() eq "POST" ) {
         my $db = connect_db();
         my $password = passphrase( params->{'pwd'} )->generate;
-        my $sql = 'insert into users (name, username, password, email) values (?, ?, ?, ?)';
+        my $sql = 'insert into users (name, author, password, email) values (?, ?, ?, ?)';
         my $sth = $db->prepare($sql) or die $db->errstr;
         $sth->execute(params->{'name'}, params->{'user'}, $password->rfc2307, params->{'email'}) or die $sth->errstr;
 
@@ -210,7 +220,29 @@ get '/logout' => needs login => sub {
     redirect '/';
 };
 
+sub _articles {
+    my $db = connect_db();
+    my $sql = 'SELECT * FROM entries ORDER BY timestamp DESC LIMIT 5';
+    my $sth = $db->prepare($sql) or die $db->errstr;
+    $sth->execute() or die $DBI::errstr;
+    my @ans = ();
+    while(my $article= $sth->fetchrow_hashref())
+    {
+        push @ans, $article;
+    }
+    return \@ans;
+}
+
+get '/feed' => sub {
+    my $feed;
+    my $articles = _articles();
+    $feed = create_atom_feed(
+            title   => 'Dancer Blog Feed',
+            entries => $articles,
+        );
+    return $feed;
+};
+
 init_db();
 start();
 true;
-
